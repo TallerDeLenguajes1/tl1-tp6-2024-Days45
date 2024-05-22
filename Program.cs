97e252eb0a819e8e2c107a9da8a854ba69958f4b
Days45 <castro.days45@gmail.com>
2024-05-22 14:35:23 -0300

    merge de ej2 y 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿/**Console.WriteLine("Hello, World!");
+﻿/**
+Console.WriteLine("Hello, World!");
 int a = 10;
 int b = a;
 Console.WriteLine("Valor de a:" + a);
@@ -25,40 +26,40 @@ if (double.TryParse(num, out double number))
 else
 {
     Console.WriteLine("El texto ingresado no es un numero.");
-}*/
-/*
+}**/
+/**
 Ejercicio 2. Ingrese al branch CalculadoraV1 y construya un programa que sea una
 calculadora que permita al usuario realizar las 4 operaciones básicas (Sumar, Restar,
 Multiplicar y Dividir) a partir de un menú para seleccionar la opción a elegir y que luego
 pida dos números y que devuelva el resultado de la operación seleccionada. Además
 una vez que termine de realizar la operación le pregunte si desea realizar otro cálculo.
-*/
+**/
 bool siguiente = true;
-bool acertado;
+bool acertado,esNumeroValido, opcionValida,numValido, valido;
 string opcion, num1Text, num2Text;
 int opciones;
-double num1, num2;
-
+double num1, num2, num;
 while (siguiente)
 {
     Console.WriteLine("(Calculadora V1)");
-    Console.WriteLine("Seleccione una opcion.");
+    Console.WriteLine("Seleccione una opción:");
     Console.WriteLine("1. Sumar");
     Console.WriteLine("2. Restar");
     Console.WriteLine("3. Multiplicar");
     Console.WriteLine("4. Dividir");
-
+    Console.WriteLine("5. Operaciones de CalculadoraV2");
     opcion = Console.ReadLine();
-    bool esNumeroValido = int.TryParse(opcion, out opciones);
-
-    if (opciones >= 1 && opciones <= 4)
+    esNumeroValido = int.TryParse(opcion, out opciones);
+    if (esNumeroValido)
     {
-        Console.WriteLine("Ingrese el primer numero.");
+        if (opciones >= 1 && opciones <= 4)
+        {
+        Console.WriteLine("Ingrese el primer número:");
         num1Text = Console.ReadLine();
-        Console.WriteLine("Ingrese el segundo numero.");
+        Console.WriteLine("Ingrese el segundo número:");
         num2Text = Console.ReadLine();
-        acertado= double.TryParse(num1Text, out num1);
-        acertado=double.TryParse(num2Text, out num2) == acertado;
+        acertado = double.TryParse(num1Text, out num1);
+        acertado = double.TryParse(num2Text, out num2) == acertado;
 
         if (acertado)
         {
@@ -71,12 +72,12 @@ while (siguiente)
                     Console.WriteLine($"La resta es: {num1 - num2}");
                     break;
                 case 3:
-                    Console.WriteLine($"El producto es: {num1*num2}");
+                    Console.WriteLine($"El producto es: {num1 * num2}");
                     break;
                 case 4:
                     if (num2 != 0)
                     {
-                        Console.WriteLine($"La division es: {num1 / num2}");
+                        Console.WriteLine($"La división es: {num1 / num2}");
                     }
                     else
                     {
@@ -84,83 +85,93 @@ while (siguiente)
                     }
                     break;
             }
-        }
-    }
-
-    Console.WriteLine("Desea seguir? (s:si/n:no)");
-    string respuesta = Console.ReadLine();
-    siguiente = respuesta.ToLower() == "s";
-}
-*/
-
-bool siguiente = true, numValido, opcionValida;
-string opcion;
-int opciones;
-double num;
-while (siguiente)
-{
-    Console.WriteLine("(Calculadora V2) ");
-    Console.WriteLine("1. Valor absoluto.");
-    Console.WriteLine("2. Cuadrado.");
-    Console.WriteLine("3. Raiz Cuadrada.");
-    Console.WriteLine("4. Seno.");
-    Console.WriteLine("5. Coseno.");
-    Console.WriteLine("6. Parte entera de un float.");
-    opcion=Console.ReadLine();
-    opcionValida=int.TryParse( opcion, out opciones);
-    if (opcionValida)
-    {
-        if (opciones>=1 && opciones<=6)
+        }else
         {
-            Console.WriteLine("Ingrese un numero.");
-            opcion=Console.ReadLine();
-            numValido=double.TryParse(opcion, out num);
-            if (numValido)
-            {
-                switch (opciones)
-                {
-                    case 1:Console.WriteLine($"El valor absoluto de {num} es {Math.Abs(num)}");
-                    break;
-                    case 2:Console.WriteLine($"El cuadrado de {num} es {Math.Pow(num,2)}");
-                    break;
-                    case 3:Console.WriteLine($"La raiz Cuadrada de {num} es {Math.Sqrt(num)}");
-                    break;
-                    case 4:Console.WriteLine($"El seno de {num} es {Math.Sin(num)}");
-                    break;
-                    case 5:Console.WriteLine($"El coseno de {num} es {Math.Cos(num)}");
-                    break;
-                    case 6:Console.WriteLine($"La parte entera de {num} es {(int)num}");
-                    break;
-                }
-                Console.WriteLine("Ingrese el primer numero.");
-                string leer=Console.ReadLine();
-                bool valido = double.TryParse(leer, out double numero1);
-                Console.WriteLine("Ingrese el segundo numero.");
-                leer=Console.ReadLine();
-                valido=double.TryParse(leer,out double numero2 )&&valido;
-                if (valido){
-                    Console.WriteLine("Los numeros ingresados son validos, entonces...");
-                    Console.WriteLine($"El maximo es {Math.Max(numero1,numero2)}");
-                    Console.WriteLine($"El minimo es {Math.Min(numero1,numero2)}");
-                }else{
-                    Console.WriteLine("Alguno de los dos valores ingresados no son numeros.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Número inválido. Intente de nuevo.");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Opción inválida. Intente de nuevo.");
+            Console.WriteLine("Alguno o los dos valores ingresados no es un número.");
         }
     }
     else
     {
-        Console.WriteLine("Opción inválida. Intente de nuevo.");
+        if (opciones == 5)
+        {
+            Console.WriteLine("(Calculadora V2)");
+            Console.WriteLine("1. Valor absoluto");
+            Console.WriteLine("2. Cuadrado");
+            Console.WriteLine("3. Raíz Cuadrada");
+            Console.WriteLine("4. Seno");
+            Console.WriteLine("5. Coseno");
+            Console.WriteLine("6. Parte entera de un número");
+            opcion = Console.ReadLine();
+            opcionValida = int.TryParse(opcion, out opciones);
+
+            if (opcionValida)
+            {
+                if (opciones >= 1 && opciones <= 6)
+                {
+                    Console.WriteLine("Ingrese un número:");
+                    opcion = Console.ReadLine();
+                    numValido = double.TryParse(opcion, out num);
+
+                    if (numValido)
+                    {
+                        switch (opciones)
+                        {
+                            case 1:
+                                Console.WriteLine($"El valor absoluto de {num} es {Math.Abs(num)}");
+                                break;
+                            case 2:
+                                Console.WriteLine($"El cuadrado de {num} es {Math.Pow(num, 2)}");
+                                break;
+                            case 3:
+                                Console.WriteLine($"La raíz Cuadrada de {num} es {Math.Sqrt(num)}");
+                                break;
+                            case 4:
+                                Console.WriteLine($"El seno de {num} es {Math.Sin(num)}");
+                                break;
+                            case 5:
+                                Console.WriteLine($"El coseno de {num} es {Math.Cos(num)}");
+                                break;
+                            case 6:
+                                Console.WriteLine($"La parte entera de {num} es {(int)num}");
+                                break;
+                        }
+
+                        Console.WriteLine("Ingrese el primer número:");
+                        string leer = Console.ReadLine();
+                        valido = double.TryParse(leer, out double numero1);
+                        Console.WriteLine("Ingrese el segundo número:");
+                        leer = Console.ReadLine();
+                        valido = double.TryParse(leer, out double numero2) && valido;
+
+                        if (valido)
+                        {
+                            Console.WriteLine("Los números ingresados son válidos, entonces...");
+                            Console.WriteLine($"El máximo es {Math.Max(numero1, numero2)}");
+                            Console.WriteLine($"El mínimo es {Math.Min(numero1, numero2)}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Alguno o los dos valores ingresados no es un número.");
+                        }
+                    }else
+                    {
+                        Console.WriteLine("Entrada invalida. Intente de nuevo.");
+                    }
+                }else
+                {
+                    Console.WriteLine("Opción inválida. Intente de nuevo.");
+                }
+            }else
+            {
+                Console.WriteLine("Entrada invalida. Intente de nuevo.");
+            }
+        }else
+        {
+            Console.WriteLine("Opcion invalida. Intente de nuevo.");
+        }
     }
-    Console.WriteLine("Desea seguir? (s:si/n:no)");
-    string respuesta = Console.ReadLine();
-    siguiente = respuesta.ToLower() == "s";
+    }else
+    {
+        Console.WriteLine("Entrada invalida. Intente de nuevo.");
+    }
 }
